c5e73a5e698182339775848b2e821a9f7ec92fd2
ik09506 <ik09506@7ea8e8cd-8cd7-493b-96f7-4414cfa45442>
2020-03-30 12:27:52 +0000

    cas bleu de P2 a été ajouté

--- TRUNK/P2Main.java
+++ TRUNK/P2Main.java
@@ -17,36 +17,73 @@ public class P2Main {
 		do { 
 			robot.getColor().colorScan();
 		}while ((!(robot.ToucherDeux())) && (robot.getColor().getCouleurCourant()!= "GREEN" && robot.getColor().getCouleurCourant() != "BLUE")); 
-		robot.RotateUntilLinePerp("GREEN", "Right");
-		// Si c'est VERT:
-		robot.parcourirLigne("GREEN");//on parcour la ligne verte
-		if(robot.getGo()) {// si go est vrai ça veut dire que le robot n'a pas croiisé de palet
-			robot.getRoues().RotateD("Right", 90);// vu qu'il n'y a pas de palet on tourne en deux étapes pour parcourir le reste de la ligne
-			if(robot.getGo()) {
-				robot.RotateUntilLine("GREEN","Right");
+		
+		if(robot.getColor().getCouleurCourant()=="GREEN") {	// Si c'est VERT:
+			robot.RotateUntilLinePerp("GREEN", "Right");
+		
+			robot.parcourirLigne("GREEN");//on parcour la ligne verte
+			if(robot.getGo()) {// si go est vrai ça veut dire que le robot n'a pas croiisé de palet
+				robot.getRoues().RotateD("Right", 90);// vu qu'il n'y a pas de palet on tourne en deux étapes pour parcourir le reste de la ligne
 				if(robot.getGo()) {
-					robot.parcourirLigne("GREEN");// on parcourt lereste de la ligne verte
+					robot.RotateUntilLine("GREEN","Right");
 					if(robot.getGo()) {
-						robot.getRoues().RotateD("Right", 90);// le robot tourne a 90 degre pour rejoindre la ligne noire
+						robot.parcourirLigne("GREEN");// on parcourt lereste de la ligne verte
 						if(robot.getGo()) {
-							robot.rejoindreLigne("BLACK", "Right");// lorsqu'il arrive a la ligne noire il tourne à droite
+							robot.getRoues().RotateD("Right", 90);// le robot tourne a 90 degre pour rejoindre la ligne noire
 							if(robot.getGo()) {
-								robot.parcourirLigne("BLACK");// le robot parcourt l'intégralité de la ligne noire 
+								robot.rejoindreLigne("BLACK", "Right");// lorsqu'il arrive a la ligne noire il tourne à droite
 								if(robot.getGo()) {
-									robot.getRoues().RotateD("Left", 90);// il tourne à gauche 
+									robot.parcourirLigne("BLACK");// le robot parcourt l'intégralité de la ligne noire 
 									if(robot.getGo()) {
-										robot.rejoindreLigne("BLUE", "Left");// il rejoint la ligne bleue
+										robot.getRoues().RotateD("Left", 90);// il tourne à gauche 
 										if(robot.getGo()) {
-											robot.parcourirLigne("BLUE");// il parcourt la ligne bleue
-										}	
+											robot.rejoindreLigne("BLUE", "Left");// il rejoint la ligne bleue
+											if(robot.getGo()) {
+												robot.parcourirLigne("BLUE");// il parcourt la ligne bleue
+											}	
+										}
 									}
 								}
 							}
 						}
 					}
 				}
+				robot.getRoues().Stop();// à la fin de la ligne, le robot s'arrete
 			}
-			robot.getRoues().Stop();// à la fin de la ligne, le robot s'arrete
 		}
-	}
+		
+		else if (robot.getColor().getCouleurCourant()=="BLUE") {
+			robot.RotateUntilLinePerp("BLUE", "Right");
+			
+			robot.parcourirLigne("BLUE");//on parcour la ligne verte
+			if(robot.getGo()) {// si go est vrai ça veut dire que le robot n'a pas croiisé de palet
+				robot.getRoues().RotateD("Right", 90);// vu qu'il n'y a pas de palet on tourne en deux étapes pour parcourir le reste de la ligne
+				if(robot.getGo()) {
+					robot.RotateUntilLine("BLUE","Right");
+					if(robot.getGo()) {
+						robot.parcourirLigne("BLUE");// on parcourt lereste de la ligne verte
+						if(robot.getGo()) {
+							robot.getRoues().RotateD("Right", 90);// le robot tourne a 90 degre pour rejoindre la ligne noire
+							if(robot.getGo()) {
+								robot.rejoindreLigne("BLACK", "Right");// lorsqu'il arrive a la ligne noire il tourne à droite
+								if(robot.getGo()) {
+									robot.parcourirLigne("BLACK");// le robot parcourt l'intégralité de la ligne noire 
+									if(robot.getGo()) {
+										robot.getRoues().RotateD("Left", 90);// il tourne à gauche 
+										if(robot.getGo()) {
+											robot.rejoindreLigne("GREEN", "Left");// il rejoint la ligne bleue
+											if(robot.getGo()) {
+												robot.parcourirLigne("GREEN");// il parcourt la ligne bleue
+											}	
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+				robot.getRoues().Stop();// à la fin de la ligne, le robot s'arrete
+			}
+		}
+	}	
 }
