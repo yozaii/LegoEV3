de39f429b7d2d2ba7ef78a89fe5c0c985ae1e65a
ij01479 <ij01479@7ea8e8cd-8cd7-493b-96f7-4414cfa45442>
2020-04-17 12:56:46 +0000

    Ajout de P6 et modification de P7

--- TRUNK/TestScenarioP7.java
+++ TRUNK/TestScenarioP7.java
@@ -23,7 +23,6 @@ public class TestScenarioP7 extends Robot{
 			super.getRoues().Stop();
 			super.getPinces().Close();
 			super.getRoues().RotateToZero();
-			super.StopWhite();
 		}
 		
 		if (super.getColor().getCouleurCourant()=="WHITE") {
@@ -60,6 +59,51 @@ public class TestScenarioP7 extends Robot{
 			}
 		}
 		while(robot.getGo());
+
+//On execute tous les lignes suivant apres la fin de boucle precedente (le palet est attrape)
+		
+		do {
+			robot.getRoues().MoveForwardSpeed(250f);//Le robot avance
+			robot.getColor().colorScan();//Il scan le couleur en dessous en avancant
+		}
+		while (robot.getColor().getCouleurCourant()!="WHITE" && robot.getColor().getCouleurCourant()!="BLACK");//Tant que les couleurs scanne ne sont pas noires ou blaches
+		
+		if (robot.getColor().getCouleurCourant()=="BLACK") {//Si le couleur est noire
+			do {
+				robot.getRoues().MoveForwardSpeed(250f);
+				robot.getColor().colorScan();
+			}
+			while (robot.getColor().getCouleurCourant()!=EndCamp && robot.getColor().getCouleurCourant()!=StartCamp );//On avance jusqu'a la ligne de camp depart ou de camp oppose
+			
+			if (robot.getColor().getCouleurCourant()==StartCamp) {//Si le couleur detecte est celui du camp de depart
+				robot.getRoues().RotateD("Left", 180);//On fait un demitour
+				robot.StopWhite();//On avance et on pose le palet a la ligne blanche en face
+			}
+			else if (robot.getColor().getCouleurCourant()==EndCamp) {//Sinon si le couleur est celui du camp adverse
+				robot.StopWhite();//On avance et on pose le palet a la ligne blanche en face
+			}
+		}
+		else if (robot.getColor().getCouleurCourant()=="WHITE") {//Si le couleur est blanc
+			robot.getRoues().RotateD("Left", 180);//On fait un demitour
+			do {
+				robot.getRoues().MoveForwardSpeed(250f);
+				robot.getColor().colorScan();
+			}
+			while (robot.getColor().getCouleurCourant()!="BLACK");//On avance jusqu'a la ligne noire.
+			do {
+				robot.getRoues().MoveForwardSpeed(250f);
+				robot.getColor().colorScan();
+			}
+			while (robot.getColor().getCouleurCourant()!=EndCamp && robot.getColor().getCouleurCourant()!=StartCamp );
+			
+			if (robot.getColor().getCouleurCourant()==StartCamp) {
+				robot.getRoues().RotateD("Left", 180);
+				robot.StopWhite();
+			}
+			else if (robot.getColor().getCouleurCourant()==EndCamp) {
+				robot.StopWhite();
+			}
+		}
 	}
 
 }
