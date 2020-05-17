d133a98692c01302f9465100692b26d24549135e
ik09506 <ik09506@7ea8e8cd-8cd7-493b-96f7-4414cfa45442>
2020-05-17 23:53:56 +0000

    doc interne movement

--- TRUNK/Movement.java
+++ TRUNK/Movement.java
@@ -3,6 +3,11 @@ package real;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 
+/**Movement permet de manipuler les roues
+ * lMotor et rMotor indique au programme que le robot contient des roues qu'il peut utiliser => les deux moteurs doivent être synchronisés
+ * orrien garde en mémoire l'orrientation du robot en fonction de ses rotations
+ * vitesse indique la vitesse des roues
+ */
 public class Movement {
 	
 	private EV3LargeRegulatedMotor lMotor ; //Declaration d'une variable EV3RegulatedMotor lMotor (left motor)
@@ -19,7 +24,8 @@ public class Movement {
 		vitesse = 250f;
 	}
 	
-	//Méthode pour avancer, vitesse deja fixée
+	/**Méthode pour avancer, vitesse deja fixée
+	 */
 	public void MoveForward() {
 		float dps = 480f; 		// float degrés de rotation par seconde
 		lMotor.setSpeed(dps);	//vitesse de rotation lMotor = dps
@@ -30,7 +36,9 @@ public class Movement {
 		lMotor.endSynchronization();
 	}
 	
-	//Methode pour avancer. Prend en argument float speed = rotations par seconde
+	/**Methode pour avancer. 
+	 * @param float speed (rotations par seconde)
+	 */
 	public void MoveForwardSpeed(float speed) {
 		lMotor.setSpeed(speed);	//vitesse de rotation lMotor = speed;
 		rMotor.setSpeed(speed);
@@ -40,7 +48,9 @@ public class Movement {
 		lMotor.endSynchronization();
 	}
 	
-	//Methode pour avancer. Prend en argument deg = au nombre de degre de rotation de chaque roue
+	/**Méthode pour avancer. 
+	 *@param deg (nombre de degré de rotation de chaque roue)
+	 */
 	public void MoveForwardDeg(int deg) {
 		lMotor.startSynchronization();
 		lMotor.rotate(deg);
@@ -51,7 +61,8 @@ public class Movement {
 		
 	}
 	
-	//Fait reculer le robot
+	/**Fait reculer le robot
+	 */
 	public void MoveBackward() {
 		float dps = 480f; 		
 		lMotor.setSpeed(dps);	
@@ -62,15 +73,17 @@ public class Movement {
 		lMotor.endSynchronization();
 	}
 	
-	
-	public void Stop() {	//Méthode qui arrête les deux moteurs
+/**Méthode qui arrête les deux moteurs
+ */
+	public void Stop() {	
 		lMotor.startSynchronization();
 		lMotor.stop();
 		rMotor.stop();
 		lMotor.endSynchronization();
 	}
 	
-	//Methode de faire une rotation au sens des aiguilles d'une montre
+	/**Methode de faire une rotation au sens des aiguilles d'une montre
+	 */
 	public void RotateClockwise() {
 		lMotor.setSpeed(200);
 		rMotor.setSpeed(200);
@@ -80,7 +93,8 @@ public class Movement {
 		lMotor.endSynchronization();
 	}
 	
-	//Methode pour faire une rotation au sens contraire des aiguilles d'une montre
+	/**Methode pour faire une rotation au sens contraire des aiguilles d'une montre
+	 */
 	public void RotateCounterClockwise() {
 		lMotor.setSpeed(200);
 		rMotor.setSpeed(200);
@@ -90,9 +104,11 @@ public class Movement {
 		lMotor.endSynchronization();
 	}
 	
-	/*La méthode RotateD prend en argument un String Direction("Left" ou "Right") et
+	/**La méthode RotateD prend en argument un String Direction("Left" ou "Right") et
 	 *un int degre (90 ou 180). Elle fait une rotation dans le direction et degre desiré.
-	 *Exemple d'appel: RotateD("Left",90) */
+	 *Exemple d'appel: RotateD("Left",90) 
+	 *@param Direction
+	 */
 	public void RotateD(String Direction,int deg){
 		lMotor.startSynchronization();
 		if (deg==90) {	//Les rotations de 90 degres
@@ -135,7 +151,9 @@ public class Movement {
 		rMotor.waitComplete();
 	}
 	
-	//Avance en courbe à droite ou à gauche. Prend la direction desirée en argument
+	/**Avance en courbe à droite ou à gauche. Prend la direction desirée en argument
+	 * @param Direction
+	 */
 	public void arcDirection(String Direction) {
 		float dps=213;
 		lMotor.startSynchronization();
@@ -156,7 +174,8 @@ public class Movement {
 		}
 		lMotor.endSynchronization();
 	}
-	
+	/**Setter
+	 */
 	public void setOrrien(int x) {
         orrien+=x;
         if(orrien>=360) {
@@ -171,7 +190,8 @@ public class Movement {
 		orrien = 0;
 	}
 	
-	//Getters
+	/**Getters
+	 */
 	public int getOrrien(){
         return orrien;
 	}
@@ -185,13 +205,18 @@ public class Movement {
 	}
 	//Fin getters
 	
-	public void RotateToZero(){//fait tourner le robot jusqu'à que son orientation soit à 0 (que le robot soit face au camp opposé).
+/**fait tourner le robot jusqu'à que son orientation soit à 0 (que le robot soit face au camp opposé).
+ * 
+ */
+	public void RotateToZero(){
 		if (this.getOrrien() == 90) this.RotateD("Left", 90);
 		else if (this.getOrrien()==180) this.RotateD("Right", 180);
 		else if (this.getOrrien()==270) this.RotateD("Right", 90);
 	}
 	
-	public void RotateTo180(){//fait tourner le robot jusqu'à que son orientation soit à 180.
+/**fait tourner le robot jusqu'à que son orientation soit à 180.
+ */
+	public void RotateTo180(){
 		if (this.getOrrien() == 90) this.RotateD("Right", 90);
 		else if (this.getOrrien()==180) this.RotateD("Right", 180);
 		else if (this.getOrrien()==270) this.RotateD("Left", 90);
