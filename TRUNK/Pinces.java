3a6571e59e95728d2a7c2f72b85a60efb40c3f7b
ij08332 <ij08332@7ea8e8cd-8cd7-493b-96f7-4414cfa45442>
2020-05-18 00:16:45 +0000

    doc interne

--- TRUNK/Pinces.java
+++ TRUNK/Pinces.java
@@ -1,33 +1,36 @@
 package real;
-
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.utility.Delay;
 
+/** la classe Pinces permet au robot douvrir et fermer ses pinces
+ * les attributs sont:
+ *  degreOpenClose determine le nombre de degres requis par le moteur afin douvrir les pinces
+ *  cMotor 
+ */
 public class Pinces {
-	private EV3MediumRegulatedMotor cMotor;
-	private int degreOpenClose = 2560; /*Nombres de degrÃ©s requis par le moteur pour ouvrir les pinces
-	 									presque complÃ¨tement si ils Ã©taient fermÃ©s. Le contraire est
-	 									applicable aussi si on prend la valeur nÃ©gative de degreOpenClose*/
-	public Pinces() {
-		cMotor = new EV3MediumRegulatedMotor (MotorPort.C);
-		cMotor.setSpeed(1080f);
-	}
-	
-	public void Open() {
-		cMotor.rotate(degreOpenClose);
-		Delay.msDelay(100);
-		cMotor.stop();
-	}
-	
-	public void Close() {
-		cMotor.rotate(-degreOpenClose);
-		Delay.msDelay(100);
-		cMotor.stop();
-	}
-	
-	public void Stop()	{
-		cMotor.stop();
-	}
-
+        private EV3MediumRegulatedMotor cMotor;
+        private int degreOpenClose = 2560; /*Nombres de degrés requis par le moteur pour ouvrir les pinces
+                                                                                 presque complètement si ils étaient fermés. Le contraire est
+                                                                                 applicable aussi si on prend la valeur négative de degreOpenClose*/
+        public Pinces() {
+                cMotor = new EV3MediumRegulatedMotor (MotorPort.C);
+                cMotor.setSpeed(1080f);
+        }
+        
+        public void Open() {// permet louverture des pinces
+                cMotor.rotate(degreOpenClose);
+                Delay.msDelay(100);
+                cMotor.stop();
+        }
+        
+        public void Close() {// permet la fermuture des pinces
+                cMotor.rotate(-degreOpenClose);
+                Delay.msDelay(100);
+                cMotor.stop();
+        }
+        
+        public void Stop()        {// permet larret
+                cMotor.stop();
+        }
 }
