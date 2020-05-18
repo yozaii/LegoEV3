529e406d15034d0103600118b315d920e067398f
ij08332 <ij08332@7ea8e8cd-8cd7-493b-96f7-4414cfa45442>
2020-05-18 00:25:45 +0000

    doc interne StockInfo

--- TRUNK/StockInfo.java
+++ TRUNK/StockInfo.java
@@ -1,91 +1,89 @@
 package real;
-
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.Button;
 
+/**La classe StockInfo permet l'affichage du menu sur linterface du robot
+ * l'attribut campOpp indique le camp adverse soit le camp ou doit etre depose le palet
+ * l'attribut campDep indique le camp de depart
+ */
+    
 public class StockInfo {
-	
-	private String campOpp; //camp oppos? o? le robot devrai d?poser le palet
-	private String campDep; //camp de d?part du robot
 
-	//constructeur        
-	public StockInfo (){
-	
-		this.campOpp = null;
-		this.campDep = null;
-
-	}
-	
-	
-	public String getCampDep() {
-		return campDep;
-	}
-	
-	//affichage d'un interface de menu avec un fleche qui pointe sur vert
-	public void afficheCamp1() {
-		System.out.println();
-		System.out.println("  -Camp Depart?-");
-		System.out.println("  >Vert");
-		System.out.println("   Bleu");
-		System.out.println();
-		System.out.println();
-		System.out.println(" ESCAPE TO EXIT");
-		
-	}
-	
-	//affichage d'un interface de menu avec un fleche qui pointe sur bleu
-	public void afficheCamp2()	{
-		System.out.println();
-		System.out.println("  -Camp Depart?-");
-		System.out.println("   Vert");
-		System.out.println("  >Bleu");
-		System.out.println();
-		System.out.println();
-		System.out.println(" ESCAPE TO EXIT");
-	}
-
-	public void choisircamp(){ // permet de definir quel camp a ete choisi
-		
-		Button.waitForAnyPress();
-		if(Button.UP.isDown()){
-			campDep= "Ouest";
-			campOpp= "Est";
-		}
-		else if(Button.DOWN.isDown()) {
-			campDep= "Est";
-			campOpp= "Ouest";
-		}
-			
-	}
-	
-	public void menu() {
-		
-		int i =0;
-		while (Button.ESCAPE.isUp() && Button.ENTER.isUp()) {//Tant que les boutons ENTER et ESCAPE ne sont pas appuye
-			if (i%2==0) {//Si i est positif on affiche afficheCamp1
-				afficheCamp1();
-				campDep= "Est"; //Est est affecte au CampDep
-				campOpp= "Ouest";//Ouest est affecte au CampOpp
-				
-			}
-			else {//sinon (i negatif) on affiche afficheCamp2
-				afficheCamp2();
-				campDep= "Ouest";//Ouest est affecte au CampOpp
-				campOpp= "Est";//Est est affecte au CampDep
-			}
-			Button.waitForAnyPress();//On attend un appuie de bouton par l'utilisateur apres l'affichage
-			if (Button.DOWN.isDown() || Button.UP.isDown()) {
-				i++;/*A chaque appuie de bouton en haut ou en bas on incremente i par 1. Cela nous permet de changer l'affichage
-						entre afficheCamp1 et afficheCamp2 si ces boutons sont appuyes*/
-			}
-			else if (Button.ESCAPE.isDown()) {//
-				campDep = null;
-				campOpp = null;
-			}
-			
-		}
-	}
+        private String campOpp; //camp oppose ou le robot devrai deposer le palet
+        private String campDep; //camp de depart du robot
+        //constructeur        
+        public StockInfo (){
+        
+                this.campOpp = null;
+                this.campDep = null;
+        }
+        
+        
+        public String getCampDep() {
+                return campDep;
+        }
+        
+        //affichage d'un interface de menu avec un fleche qui pointe sur vert
+        public void afficheCamp1() {
+                System.out.println();
+                System.out.println("  -Camp Depart?-");
+                System.out.println("  >Vert");
+                System.out.println("   Bleu");
+                System.out.println();
+                System.out.println();
+                System.out.println(" ESCAPE TO EXIT");
+                
+        }
+        
+        //affichage d'un interface de menu avec un fleche qui pointe sur bleu
+        public void afficheCamp2()        {
+                System.out.println();
+                System.out.println("  -Camp Depart?-");
+                System.out.println("   Vert");
+                System.out.println("  >Bleu");
+                System.out.println();
+                System.out.println();
+                System.out.println(" ESCAPE TO EXIT");
+        }
+        public void choisircamp(){ // permet de definir quel camp a ete choisi
+                
+                Button.waitForAnyPress();
+                if(Button.UP.isDown()){
+                        campDep= "Ouest";
+                        campOpp= "Est";
+                }
+                else if(Button.DOWN.isDown()) {
+                        campDep= "Est";
+                        campOpp= "Ouest";
+                }
+                        
+        }
+        
+        public void menu() {
+                
+                int i =0;
+                while (Button.ESCAPE.isUp() && Button.ENTER.isUp()) {//Tant que les boutons ENTER et ESCAPE ne sont pas appuye
+                        if (i%2==0) {//Si i est positif on affiche afficheCamp1
+                                afficheCamp1();
+                                campDep= "Est"; //Est est affecte au CampDep
+                                campOpp= "Ouest";//Ouest est affecte au CampOpp
+                                
+                        }
+                        else {//sinon (i negatif) on affiche afficheCamp2
+                                afficheCamp2();
+                                campDep= "Ouest";//Ouest est affecte au CampOpp
+                                campOpp= "Est";//Est est affecte au CampDep
+                        }
+                        Button.waitForAnyPress();//On attend un appuie de bouton par l'utilisateur apres l'affichage
+                        if (Button.DOWN.isDown() || Button.UP.isDown()) {
+                                i++;/*A chaque appuie de bouton en haut ou en bas on incremente i par 1. Cela nous permet de changer l'affichage
+                                                entre afficheCamp1 et afficheCamp2 si ces boutons sont appuyes*/
+                        }
+                        else if (Button.ESCAPE.isDown()) {//
+                                campDep = null;
+                                campOpp = null;
+                        }
+                        
+                }
+        }
 }
-
-
-
