c2a0d9ba3914c8a0679c8f3d702b0af63c55512b
ij08332 <ij08332@7ea8e8cd-8cd7-493b-96f7-4414cfa45442>
2020-05-18 00:38:01 +0000

    doc interne TouchSensor

--- TRUNK/TouchSensor.java
+++ TRUNK/TouchSensor.java
@@ -1,20 +1,20 @@
 package real;
-
 import lejos.hardware.port.Port;
 import lejos.hardware.sensor.EV3TouchSensor;
-
-
 public class TouchSensor extends EV3TouchSensor {
-	
-	public TouchSensor(Port port) {
-		super(port);
-	}
-	
-	
-	public boolean isPressed() {
-		float [] sample = new float[1];
-		fetchSample(sample, 0);
-		return sample[0] !=0;
-		
-	}
+	/** La classe TouchSensor controle le capteur tactile. Elle permet au robot de detecter la presence dun palet
+
+	*/
+        
+        public TouchSensor(Port port) {// cette class permet au robot de detecter la presence dun palet
+                super(port);
+        }
+        
+        
+        public boolean isPressed() {// cette methode renvoi un booleen: si le capteur est touche elle renvoi true et false sinon
+                float [] sample = new float[1];
+                fetchSample(sample, 0);
+                return sample[0] !=0;
+                
+        }
 }
